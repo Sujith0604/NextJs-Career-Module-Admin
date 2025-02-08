2eefac848ab37d0ef487badf0d23e5abbecd4746
Sujith Karthikaiselvan <sujithkarthikaiselvan@gmail.com>
2025-02-08 11:56:49 +0530

    any

--- app/__components/DeletePostButton.tsx
+++ app/__components/DeletePostButton.tsx
@@ -1,5 +1,5 @@
 "use client";
-const DeletePostButton = ({ postId }) => {
+const DeletePostButton = ({ postId: any }) => {
   const handleClick = async () => {
     try {
       await fetch(`/api/post/${postId}`, {
